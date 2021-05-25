0e0668dae817c538c98cceb20fb159ff349dd054
sudeepkhandekar99 <sudeepkhandekar99@gmail.com>
2021-05-25 15:57:01 +0530

    find empty and validity functions

--- working.py
+++ working.py
@@ -10,6 +10,19 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+
+def valid(bo, num, pos):
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    for j in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    
+
+
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
@@ -24,6 +37,10 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-print_board(board)
 
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo)[0]):
+            if bo[i][j] == 0:
+                return (i, j)
 
