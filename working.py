7f794e4d938f67aeefcfa6f3c80799c55bf6b708
sudeepkhandekar99 <sudeepkhandekar99@gmail.com>
2021-05-25 16:05:02 +0530

    entering values function

--- working.py
+++ working.py
@@ -20,7 +20,16 @@ def valid(bo, num, pos):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
 
-    
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False    
+
+    return True
+
 
 
 def print_board(bo):
